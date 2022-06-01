705e73c253fa1a5c340f77db146d7824244bb64f
Alejandro Campos <acamposcar@gmail.com>
2022-06-02 01:28:21 +0200

    Add alerts and timer

--- client/src/pages/Level.js
+++ client/src/pages/Level.js
@@ -8,6 +8,9 @@ const Level = ({ levels }) => {
   const [currentLevel, setCurrentLevel] = useState('')
   const [showDialog, setShowDialog] = useState(false)
   const [dialogPosition, setDialogPosition] = useState({})
+  const [timer, setTimer] = useState(0)
+  const [showInvalidAlert, setShowInvalidAlert] = useState(false)
+  const [showValidAlert, setShowValidAlert] = useState(false)
   const [relativeClickPosition, setRelativeClickPosition] = useState({})
 
   useEffect(() => {
@@ -18,12 +21,18 @@ const Level = ({ levels }) => {
     })
   }, [levels, title])
 
+  useEffect(() => {
+    const interval = setInterval(() => setTimer(prevState => prevState + 1), 1000)
+    return () => clearInterval(interval)
+  }, [])
+
   const getRelativeClickPosition = (rect, relativeLeft, relativeTop) => {
     const currentImageHeight = rect.height
     const currentImageWidth = rect.width
     setRelativeClickPosition({
       top: relativeTop / currentImageHeight, left: relativeLeft / currentImageWidth
     })
+    console.log(relativeClickPosition)
   }
 
   const toggleDialog = (e) => {
@@ -36,18 +45,39 @@ const Level = ({ levels }) => {
   }
 
   const handleSelection = (e) => {
-    console.log(e.target.value)
     const [itemSelected] = currentLevel.items.filter(item => item.name === e.target.value)
     if (Math.abs(relativeClickPosition.top - itemSelected.top) < 0.05 && Math.abs(relativeClickPosition.left - itemSelected.left) < 0.05) {
-      console.log('found')
+      setShowInvalidAlert(false)
+      setShowValidAlert(true)
+      setTimeout(() => setShowValidAlert(false), 2000)
+
+      setCurrentLevel(prevState => {
+        const updatedItems = prevState.items.map(item => {
+          if (item.name === itemSelected.name) {
+            return { ...item, found: true }
+          }
+          return item
+        })
+        return {
+          ...prevState, items: updatedItems
+        }
+      })
+    } else {
+      setShowInvalidAlert(true)
+      setShowValidAlert(false)
+      setTimeout(() => setShowInvalidAlert(false), 2000)
     }
     setShowDialog(prevState => !prevState)
+  }
+  const formatSeconds = (seconds) => {
+    return new Date(seconds * 1000).toISOString().substring(14, 19)
   }
 
   return (
     <>
+      {formatSeconds(timer)}
       {showDialog ? <Dialog level={currentLevel} position={dialogPosition} onSelection={handleSelection} /> : ''}
-      <CharactersHeader level={currentLevel} />
+      <CharactersHeader characters={currentLevel.items} showInvalid={showInvalidAlert} showValid={showValidAlert} />
       <img className={classes.image} src={`/images/${currentLevel.image}`} alt={currentLevel.title} onClick={toggleDialog} />
     </>
   )
